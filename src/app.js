5468507702e302f59447db9a965de44b769f69d0
Sagar Thorat (Cenitex) <sagar.thorat@cenitex.vic.gov.au>
2021-04-29 14:19:48 +1000

    added 404 error handling

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 const path = require('path');
 const express = require('express');
-
+const hbs = require('hbs');
 const app = express(); //initialize express
+
+//Define paths for Express config
+const publicDirPath = path.join(__dirname, '../public');
+const viewPaths = path.join(__dirname,'../templates/views'); //path where hbs views are stored
+const partialPaths = path.join(__dirname,'../templates/partials');
+hbs.registerPartials(partialPaths);
+
+//Setup handle bar engines and views location
+app.set('views', viewPaths);
 app.set('view engine','hbs')//setting handlebar with express for dynamic content
 
-const publicDirPath = path.join(__dirname, '../public');
-
+//Setup static directory to serve
 app.use(express.static(publicDirPath));
+
+
 app.get('',(req,res)=>{
 
     res.render('index',{
-        title: 'Weather-App',
+        title: 'Weather-Application',
         name: 'Node JS'
 
     }); //render dynamic contents using hbs
@@ -37,6 +47,27 @@ app.get('/help',(req,res)=>{
 
 })
 
+//If the resource doesn't exist, throw 404 error
+app.get('/help/*',(req,res)=>{
+
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Help article not found'
+    });
+
+})
+
+app.get('*',(req,res)=>{
+
+    res.render('404', {
+        title: '404 Page not found',
+        errorMessage: 'Page not found'
+    });
+
+    }); //render dynamic contents using hbs
+
+
+//start the express web server and listen on port 3000
 app.listen(3000, () => {
 
     console.log("Server is running");
